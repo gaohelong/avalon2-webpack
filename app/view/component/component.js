3e20c7d0fe6e0bfd7f3f4108898e59dedf5d3a73
helong.gao <helong.gao@sinobbd.com>
2017-06-28 20:27:34 +0800

    avalon2-webpack: component

--- app/view/component/component.js
+++ app/view/component/component.js
@@ -1,9 +1,15 @@
 /**
  * @desc component-soleSlot
  */
+var msButtonVM; // 组件通信.
 avalon.component('ms-button', {
-    template: '<button type="button" class="hl-btn hl-mgn-b-10"><span><slot /></span></button>',
+    template: '<button type="button" class="hl-btn hl-mgn-b-10">\
+                   <span><slot /></span>\
+               </button>',
     defaults: {
+        onInit: (e) => {
+            msButtonVM = e.vmodel;
+        },
         buttonText: "button"
     },
     soleSlot: 'buttonText'
@@ -14,9 +20,11 @@ avalon.component('ms-button', {
  */
 var msButton1VM; // 组件通信.
 avalon.component('ms-button-1', {
-    template: '<button type="button" class="hl-btn hl-mgn-b-10"><span>{{@title}}</span></button>',
+    template: '<button type="button" class="hl-btn hl-mgn-b-10">\
+                   <span>{{@title}}</span>\
+               </button>',
     defaults: {
-        onInit:function(e){
+        onInit: (e) => {
             msButton1VM = e.vmodel
         },
         title: 'button-1',
@@ -24,13 +32,46 @@ avalon.component('ms-button-1', {
 });
 
 /**
+ * @desc 多个slot
+ */
+avalon.component('ms-tabs', {
+    template: '<div>\
+                   <p>它有{{@num}}个面板</p>\
+                   <slot name="tab"/>\
+               </div>',
+    defaults: {
+        num: 3
+    }
+});
+
+/**
  * @desc component header
  */
+var msHeaderVM;
 avalon.component('ms-header', {
-    template: '',
+    template:   '<div class="hl-header hl-clearfix">\
+                    <div class="logo hl-f-l" ms-on-mouseenter="@titleEnter(@titleMIS, $event)" ms-on-mouseleave="@titleLeave(@titleAvalon, $event)">{{@title}}</div>\
+                    <div class="logout hl-f-r">\
+                        <a href="../#!/logout">退出</a>\
+                    </div>\
+                </div>',
     defaults: {
-        
-    }
+        onInit: (e) => {
+            msHeaderVM = e.vmodel;
+        },
+        onReady: (e) => {
+            console.log(this, e);
+        },
+        title: 'Avalon2',
+        titleAvalon: 'Avalon2',
+        titleMIS: 'MIS',
+        titleEnter: (name, e) => {
+            msHeaderVM.title = name;
+        },
+        titleLeave: (name, e) => {
+            msHeaderVM.title = name;
+        }
+   }
 });
 
 let vm = avalon.define({
@@ -42,12 +83,11 @@ let vm = avalon.define({
     },
 
     // soleSlot ms-button.
-    msBtnTitle: '动态内容',
     msBtnClick: () => {
         console.log('我被点击了');
     },
     updateMsBtnTtileClick: () => {
-        vm.msBtnTitle = '更新内容'+ (new Date - 0);
+        msButtonVM.buttonText = '更新内容: '+ (new Date - 0); // 组件通信.
     },
 
     // soleSlot ms-button-1.
@@ -56,9 +96,10 @@ let vm = avalon.define({
         console.log('我被点击了-1');
     },
     updateMsBtn1TtileClick: () => {
-        msButton1VM.title = '更新内容'+ (new Date - 0); // 组件通信.
+        msButton1VM.title = '更新内容-1: '+ (new Date - 0); // 组件通信.
     },
 
-    // ms-header.
+    // tabs.
 
+    // ms-header.
 });
